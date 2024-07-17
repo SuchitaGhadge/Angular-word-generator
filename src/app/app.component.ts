ca16d471c9c442ded8ea1c2834726458de8ec75b
SuchitaGhadge <suchita8494.sg@gmail.com>
2024-07-17 16:34:27 +0530

    upgrade to standalone structure using: ng generate @angular/core:standalone command

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import arrayWords from '../utils/words';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: true
 })
 export class AppComponent {
   title = 'word-generator';
